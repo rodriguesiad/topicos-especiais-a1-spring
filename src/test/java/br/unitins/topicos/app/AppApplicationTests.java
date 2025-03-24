0cea0451e106a8522db6a9affa57da50f9683598
rodriguesiad <irodriguesmoraes79@gmail.com>
2025-03-24 10:06:25 -0300

    Criação do projeto

---
+++ src/test/java/br/unitins/topicos/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unitins.topicos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

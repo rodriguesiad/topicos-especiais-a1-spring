ba73d728579d59a4b73b7c5b7ad051eceb9d6a3e
rodriguesiad <irodriguesmoraes79@gmail.com>
2025-03-31 08:46:50 -0300

    Implementa classe UserDatails utilizada pelo security para autenticação

--- src/main/java/br/unitins/topicos/app/usuario/entity/Usuario.java
+++ src/main/java/br/unitins/topicos/app/usuario/entity/Usuario.java
@@ -9,12 +9,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "usuario")
-public class Usuario extends BaseEntity {
+public class Usuario extends BaseEntity implements UserDetails {
 
     @Id
     @Column(name = "id_usuario", nullable = false)
@@ -35,5 +39,39 @@ public class Usuario extends BaseEntity {
 
     @Column(name = "senha", nullable = false, length = 100)
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 
 }

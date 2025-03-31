c5d0c359586a81c5330d39175f4af7783988d1c8
rodriguesiad <irodriguesmoraes79@gmail.com>
2025-03-31 09:25:43 -0300

    Configura SecurityFilter que intercepta requisições

--- src/main/java/br/unitins/topicos/app/configuracao/security/SecurityFilter.java
+++ src/main/java/br/unitins/topicos/app/configuracao/security/SecurityFilter.java
@@ -1,9 +1,15 @@
 package br.unitins.topicos.app.configuracao.security;
 
+import br.unitins.topicos.app.configuracao.security.token.TokenService;
+import br.unitins.topicos.app.usuario.repository.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +17,37 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = obterToken(request);
 
+        if (tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails user = userRepository.findByEmail(subject).orElseThrow(() -> new RuntimeException("Não foi possível consultar usuário pelo e-mail " + subject));
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
     }
+
+    private String obterToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            return null;
+        }
+
+        return authorizationHeader;
+    }
+
 }
--- src/main/java/br/unitins/topicos/app/usuario/repository/UsuarioRepository.java
+++ src/main/java/br/unitins/topicos/app/usuario/repository/UsuarioRepository.java
@@ -3,6 +3,7 @@ package br.unitins.topicos.app.usuario.repository;
 import br.unitins.topicos.app.base.repository.BaseRepository;
 import br.unitins.topicos.app.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,6 +15,7 @@ public interface UsuarioRepository extends BaseRepository<Usuario> {
                 SELECT u FROM Usuario u WHERE u.email = :email AND u.id <> :id ORDER BY u.dataCriacao LIMIT 1
             """)
     Optional<Usuario> findEmailDuplicado(String email, String id);
-    
+
+    Optional<UserDetails> findByEmail(String email);
 }
 

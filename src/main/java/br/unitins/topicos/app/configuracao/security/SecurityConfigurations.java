f5213b272c2da2dbfb6d1baa96cdcc1e5f74a331
rodriguesiad <irodriguesmoraes79@gmail.com>
2025-03-31 09:28:27 -0300

    Implementa SecurityConfigurations

--- src/main/java/br/unitins/topicos/app/configuracao/security/SecurityConfigurations.java
+++ src/main/java/br/unitins/topicos/app/configuracao/security/SecurityConfigurations.java
@@ -1,13 +1,17 @@
 package br.unitins.topicos.app.configuracao.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,9 +19,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfigurations implements WebMvcConfigurer {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return null;
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(HttpMethod.POST, "/login/**").permitAll();
+                    request.requestMatchers(HttpMethod.PATCH, "/login/**").permitAll();
+                    request.requestMatchers(HttpMethod.POST, "/usuarios").permitAll();
+                    request.anyRequest().authenticated();
+                })
+                .logout(logout -> {
+                    logout
+                            .logoutUrl("/logout")
+                            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                            .permitAll();
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
 }

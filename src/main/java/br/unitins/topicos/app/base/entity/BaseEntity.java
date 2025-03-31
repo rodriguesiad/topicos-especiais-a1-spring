1ce7492c3c7b0f47dcae4d373b64447ab3c113bb
rodriguesiad <irodriguesmoraes79@gmail.com>
2025-03-31 09:21:03 -0300

    Troca anotações @Getter @Setter por @Data

--- src/main/java/br/unitins/topicos/app/base/entity/BaseEntity.java
+++ src/main/java/br/unitins/topicos/app/base/entity/BaseEntity.java
@@ -5,13 +5,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @MappedSuperclass
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public abstract class BaseEntity {

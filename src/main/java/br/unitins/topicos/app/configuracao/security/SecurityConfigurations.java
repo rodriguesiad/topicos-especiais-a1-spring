89642e617de4dfff4017a17491cab332be018478
rodriguesiad <irodriguesmoraes79@gmail.com>
2025-04-06 22:35:00 -0300

    Configura bean para AuthenticationManager

--- src/main/java/br/unitins/topicos/app/configuracao/security/SecurityConfigurations.java
+++ src/main/java/br/unitins/topicos/app/configuracao/security/SecurityConfigurations.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,6 +43,11 @@ public class SecurityConfigurations implements WebMvcConfigurer {
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
